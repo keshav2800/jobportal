c38d5f300ba8d70fd31ebd1772138dc312603f7b
keshav <keshavgarg8800@gmail.com>
2025-07-23 10:54:59 +0530

    mongodb error

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully');
     } catch (error) {
+        console.log("this is not connecting");
         console.log(error);
     }
 }
